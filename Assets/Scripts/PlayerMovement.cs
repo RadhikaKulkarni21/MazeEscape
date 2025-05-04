6acf922394de0baa4b1cd0aa69189e8ff64842fc
radhi <radhika2103@gmail.com>
2025-05-04 21:52:39 +0100

    player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,21 +3,31 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField]float runSpeed = 10f;
     Vector2 moveInput;
+    Rigidbody2D GumayushiRigidBody;
 
     void Start()
     {
-        
+        GumayushiRigidBody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-
+        Run();
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
+    }
+    
+    //y is set to deafult value as to give it gravity feel
+    //We are adding new velocity each frame but for x only so y remains constant
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, GumayushiRigidBody.linearVelocity.y);
+        GumayushiRigidBody.linearVelocity = playerVelocity;
     }
 }
